1930d2ea679089a6f337d80fcb133005cd5b086e
Shawn Presser <shawnpresser@gmail.com>
2020-02-17 21:57:03 -0800

    Set minibatch_size_base to num_gpus

--- run_training.py
+++ run_training.py
@@ -53,7 +53,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     train.mirror_augment = mirror_augment
     train.image_snapshot_ticks = train.network_snapshot_ticks = 10
     sched.G_lrate_base = sched.D_lrate_base = 0.002
-    sched.minibatch_size_base = 8
+    sched.minibatch_size_base = num_gpus
     sched.minibatch_gpu_base = 1
     D_loss.gamma = 10
     metrics = [metric_defaults[x] for x in metrics]
