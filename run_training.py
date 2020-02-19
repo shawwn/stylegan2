60abba49b1401946dd740a7f5c481a341ed831a5
Shawn Presser <shawnpresser@gmail.com>
2020-02-18 23:19:16 -0800

    Let resolution be set via RESOLUTION environment var

--- run_training.py
+++ run_training.py
@@ -62,7 +62,7 @@ def run(dataset, data_dir, result_dir, config_id, num_gpus, total_kimg, gamma, m
     desc = 'stylegan2'
 
     desc += '-' + dataset
-    dataset_args = EasyDict(tfrecord_dir=dataset, resolution=64)
+    dataset_args = EasyDict(tfrecord_dir=dataset, resolution=int(os.environ['RESOLUTION']) if 'RESOLUTION' in os.environ else 64)
 
     assert num_gpus in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
     sc.num_gpus = num_gpus
