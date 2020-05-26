b1466bfe92fa38b1dccd4cd4499b8dd2a90466e9
Shawn Presser <shawnpresser@gmail.com>
2020-05-26 15:34:48 -0700

    Fix training script

--- train_ffhq_1024.sh
+++ train_ffhq_1024.sh
@@ -8,19 +8,22 @@ config="config-f" # StyleGAN 2
 #config="config-a" # StyleGAN 1
 
 data_dir=gs://dota-euw4a/datasets
-dataset=ffhq1024
+dataset=animefaces
 mirror=true
 metrics=none
 
 export TPU_HOST=10.255.128.2
 export TPU_NAME=tpu-v3-128-euw4a-50
 cores=128
+export IMAGENET_TFRECORD_DATASET='gs://dota-euw4a/datasets/ffhq1024/ffhq1024-0*'
 export MODEL_DIR=gs://dota-euw4a/runs/run76-ffhq-1024-mirror/
 export BATCH_PER=4
 export BATCH_SIZE=$(($BATCH_PER * $cores))
 export SPATIAL_PARTITION_FACTOR=2
 export RESOLUTION=1024
 export LABEL_SIZE=0
+export LABEL_BIAS=0
+export IMAGENET_UNCONDITIONAL=1
 #export LABEL_FILE=gs://arfa-euw4a/datasets/e621-cond/e621-cond-rxx.labels
 
 set -x
