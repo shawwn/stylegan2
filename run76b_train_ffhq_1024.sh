7a501ec0ab86defb0c8d4b843a308c64e8f7cceb
Shawn Presser <shawnpresser@gmail.com>
2020-05-27 00:53:31 -0700

    Fix model dir

--- run76b_train_ffhq_1024.sh
+++ run76b_train_ffhq_1024.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
 export TPU_NAME="${TPU_NAME:-tpu-v3-512-euw4a-53}"
-export RUN_NAME="${RUN_NAME:-run76-ffhq-1024-mirror}"
+export RUN_NAME="${RUN_NAME:-run76b-ffhq-1024-mirror}"
 exec bash run76_train_ffhq_1024.sh
 
